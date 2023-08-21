263298c8d430b1492f2539d5c6299e76bc2d1b8d
Mwasigwa2 <gathirimu.mwasigwa@student.moringaschool.com>
2023-08-21 12:17:42 +0300

    Done.

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
 
+# Question 1:
 def admin_login(username, password):
-    # your code here
+    if (username == "admin" or "ADMIN" and password == 12345):
+       return "Acess granted"
+    else:
+        return "Acess denied"
+    
     pass
 
+# Question 2:
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's chilly out there!"
+    elif temperature >85:
+        return "It's too dang hot!"
+    else:
+        return "It's perect out there!"
+    
     pass
-
-def fizzbuzz(num):
-    # your code here
-    pass
-
+# Question 3
+# Question 4
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+
+    else:
+        return ("Division by zero is not allowed." or "Invalid operation!")
